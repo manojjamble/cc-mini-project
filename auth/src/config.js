fa340152f1a586c3a6e5d3b07c682e546b180038
manojjamble <manoj.jamble21@pccoepune.org>
2024-11-12 00:43:09 +0530

    a

--- auth/src/config.js
+++ auth/src/config.js
@@ -1,2 +1,2 @@
-export const API = 'http://15.207.54.63:8080';
-export const UI = 'http://15.207.54.63:5173';
+export const API = 'http://13.201.189.64:8080';
+export const UI = 'http://13.201.189.64:5173';
